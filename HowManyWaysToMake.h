869bea95e9560d9a35588dc08faa4e3ba0d8b521
Viktoria Marvakova <viktoria.marvakova@kcl.ac.uk>
2016-12-07 14:30:25 +0000

    commit

--- HowManyWaysToMake.h
+++ HowManyWaysToMake.h
@@ -3,6 +3,33 @@
 
 // TODO: Your code goes here
 
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <functional>
+
+using namespace std;
+using std::cout;
+using std::endl;
+using std::vector;
+
+
+//vector<int>coins;
+vector<int>::iterator iter;
+int howManyWaysToMake(const std::list<unsigned> &coins, int change){
+    //    int howManyWaysToMake(int coinSizes,vector<int>coin)
+    //begin and end iterator, over a range of coins
+    int ha =0;
+//    vector<int> table(coinSizes+1,0);
+//    
+//    table[0] = 1;
+//    for ( auto& k : coin )      //instead of for all_of
+//        for(int j=k; j<=coinSizes; ++j)
+//            table[j] += table[j-k];
+//    int ha = table[coinSizes];
+  return ha;
+    
+}
 // Do not write any code below this line
 
 #endif
--- knights.h
+++ knights.h
@@ -18,8 +18,65 @@ pair<int,int> operator+(const pair<int,int> & a, const pair<int,int> & b) {
 
 // TODO - your code goes here
 
+vector<pair<int,int>> moves (pair<int,int> position){
+    vector<pair<int,int>> possibleMoves;
+    int one = position.first;
+    int two = position.second;
+    possibleMoves={
+        {one+1,two-2},{one+2,two-1},
+        {one+2,two+1},{one+1,two+2},
+        {one-1,two+2},{one-2,two+1},
+        {one-2,two-1},{one-1,two-2}};
+    return possibleMoves;
+}
+
+Path legal_moves (int sizeBoard, Path path, pair<int,int> pos){
+    Path newPath;
+    Path checkMoves=moves(pos);
+    copy_if(checkMoves.begin(),checkMoves.end(),std::inserter(newPath,newPath.end()),[sizeBoard,path](pair<int,int>position){
+        return (position.first<sizeBoard && position.second<sizeBoard && position.first>=0 && position.second>=0 && std::find(path.begin(), path.end(), position) ==path.end());
+        
+    });
+    return newPath;
+    
+}
 
 
+pair<Path,bool> first_tour(int sizeBoard,Path path){
+    Path newMoves=legal_moves(sizeBoard,path,path.back());
+    Path newPath=path;
+    int square = sizeBoard*sizeBoard;
+    if (path.size()==square)
+        return
+    {path,true};
+    
+    else {
+        if (newMoves.size()!=0){
+            
+            auto iter=newMoves.begin();
+            auto iterEnd = newMoves.end();
+            while(iter!=iterEnd){
+        //for
+                newPath.push_back(*iter);
+                
+                pair<Path,bool> newTour=first_tour(sizeBoard,newPath);
+                
+                if (newTour.second==true ) return newTour;
+                
+                else if (*iter==newMoves.back()) {
+                    return {path,false};
+                }
+                
+                else iter++;
+            }
+            
+        }
+        else {
+            return {path,false};
+        }
+        
+    }
+}
 
 
 // Do not edit below this line
